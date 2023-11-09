35c8e04a75bb3e7531e8d765fbfb38770d19d8fc
MintInaw <131523371+MintInaw@users.noreply.github.com>
2023-11-09 14:12:47 +0900

    Title: 특정 문자로 끝나는 문자열, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 231109/특정 문자로 끝나는 문자열/string-ending-with-specific-character.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <string.h>
+int main() {
+    char a[10][21],c;
+    int len,i;
+    for(i=0;i<10;i++)
+    {
+        scanf("%s",a[i]);
+    }
+    scanf(" %c",&c);
+    for(i=0;i<10;i++)
+    {
+        len=strlen(a[i]);
+        if(c==a[i][len-1])
+        {
+            printf("%s\n",a[i]);
+        }
+    }
+    return 0;
+}
