27e47f78091c8e6389d475d3d666c9e57cc06f3c
MintInaw <131523371+MintInaw@users.noreply.github.com>
2023-11-09 14:16:17 +0900

    Title: 특정 문자로 끝나는 문자열, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 231109/특정 문자로 끝나는 문자열/string-ending-with-specific-character.c
+++ 231109/특정 문자로 끝나는 문자열/string-ending-with-specific-character.c
@@ -2,7 +2,7 @@
 #include <string.h>
 int main() {
     char a[10][21],c;
-    int len,i;
+    int len,i,sw=0;
     for(i=0;i<10;i++)
     {
         scanf("%s",a[i]);
@@ -14,7 +14,13 @@ int main() {
         if(c==a[i][len-1])
         {
             printf("%s\n",a[i]);
+            sw=1;
         }
+        
+    }
+    if(sw==0)
+    {
+        printf("None");
     }
     return 0;
 }
