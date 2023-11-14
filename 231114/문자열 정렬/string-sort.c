f800d64ef8377a91fc3456f986b2e697df6883c5
MintInaw <131523371+MintInaw@users.noreply.github.com>
2023-11-14 17:42:15 +0900

    Title: 문자열 정렬, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 231114/문자열 정렬/string-sort.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <string.h>
+int main() {
+    char a[100];
+    int i,j,c,temp,n;
+    scanf("%s",a);
+    n=strlen(a);
+    for(i=0;i<n;i++)
+    {
+        for(j=i+1;j<n;j++)
+        {
+            if(a[i]>a[j])
+            {
+                temp=a[i];
+                a[i]=a[j];
+                a[j]=temp;
+            }
+        }
+    }
+    for(i=0;i<n;i++)
+    {
+        printf("%c",a[i]);
+    }
+    return 0;
+}
