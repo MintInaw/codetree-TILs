bf0f79ca5814f9b658462cc1f46bc08e5d23c961
MintInaw <131523371+MintInaw@users.noreply.github.com>
2023-11-14 17:17:11 +0900

    Title: 오름 내림차순 정렬, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 231114/오름 내림차순 정렬/inc-dec-sorting.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+
+int main() {
+    int a[5],i,j,c,temp;
+    scanf("%d",&c);
+    for(i=0;i<5;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=0;i<5;i++)
+    {
+        for(j=i;j<5;j++)
+        {
+            if(a[i]>a[j])
+            {
+                temp=a[i];
+                a[i]=a[j];
+                a[j]=temp;
+            }
+        }
+    }
+    for(i=0;i<5;i++)
+    {
+        printf("%d ",a[i]);
+    }
+    printf("\n");
+    for(i=0;i<5;i++)
+    {
+        for(j=i;j<5;j++)
+        {
+            if(a[i]<a[j])
+            {
+                temp=a[i];
+                a[i]=a[j];
+                a[j]=temp;
+            }
+        }
+    }
+    for(i=0;i<5;i++)
+    {
+        printf("%d ",a[i]);
+    }
+    return 0;
+}
