f6edc144c7fcd47c1088379d71c8bceb21cac1b5
MintInaw <131523371+MintInaw@users.noreply.github.com>
2023-11-30 14:19:39 +0900

    Title: 배열 놀이, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 231130/배열 놀이/play-with-array.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main() {
+    int j,n,q,i,q1;
+    int  c,cnt=0,*p;
+    scanf("%d %d",&n,&q);
+    p=(int *)malloc(sizeof(int)*n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&p[i]);   
+    }
+    for(i=0;i<q;i++)
+    {
+        scanf("%d",&q1);
+        if(q1==1)
+        {
+            int a;
+            scanf("%d",&a);
+            printf("%d\n",p[a-1]);
+        }
+        else if(q1==2)
+        {
+            int a;
+            scanf("%d",&a);
+            for(j=0;j<n;j++)
+            {
+                if(p[j]==a)
+                {
+                    cnt=j;
+                    break;
+                }
+            }
+             if(cnt>0)
+                {
+                    printf("%d\n",cnt+1);
+                }
+                else
+                {
+                    printf("0\n");
+                }
+        }
+        else
+        {
+            int a,b;
+            scanf("%d %d",&a,&b);
+            for(j=a;j<=b;j++)
+            {
+                printf("%d ",p[j-1]);
+            }
+            printf("\n");
+        }
+
+    }
+    return 0;
+}
