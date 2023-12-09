8891911a269466e88f03b6ff60d917faacabb738
MintInaw <131523371+MintInaw@users.noreply.github.com>
2023-12-09 22:32:56 +0900

    Title: 단어로 구분하기 3, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 231209/단어로 구분하기 3/separate-words-with-words-3.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+int main() {
+    char a[10][21];
+    int i;
+    for(i=0;i<10;i++)
+    {
+        scanf("%s",a[i]);
+    }
+    for(i=9;i>-1;i--)
+    {
+        printf("%s\n",a[i]);
+    }
+    return 0;
+}
