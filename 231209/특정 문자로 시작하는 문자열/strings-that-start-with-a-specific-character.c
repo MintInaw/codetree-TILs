bd6e9cf4e19631005eb86ee51f84240cd110fe89
MintInaw <131523371+MintInaw@users.noreply.github.com>
2023-12-09 22:48:47 +0900

    Title: 특정 문자로 시작하는 문자열, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 231209/특정 문자로 시작하는 문자열/strings-that-start-with-a-specific-character.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+int main() {
+    char a[20][21],b;
+    int n,i,j,cnt=0;
+    double sum=0;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%s",a[i]);
+    }
+    scanf("%c",b);
+    for(i=0;i<n;i++)
+    {
+        for(j=0;j<21;j++)
+        {
+            if(a[i][j]==b)
+            {
+                cnt++;
+                sum+=strlen(a[i]);
+                break;
+            }
+        }
+    }
+    printf("%d %.2lf",cnt-1,round(sum/cnt));
+    return 0;
+}
